cd954455ecb57462d1548d35406e294fa43546a4
132nd-Entropy <ber17118@gmail.com>
2017-11-15 15:22:35 +0100

    new: Added Civilian Airtraffic in Russian Airspace

---
+++ l10n/DEFAULT/RandomAirTraffic.lua
@@ -0,0 +1,31 @@
+
+
+-- Create RAT object. Additionally, to the template group name we give the group an alias to be able to distinguish to another group created from this template.
+RussianYak=RAT:New("Russian Civilian Airtraffic")
+
+
+-- Change coalition of Yak to blue.
+RussianYak:SetCoalitionAircraft("red")
+
+
+-- This restricts the possible departure and destination airports the airports belonging to the red coalition.
+-- Here it is important that in the mission editor enough (>2) airports have been set to red! Otherwise there will be no possible departure and/or destination airports.
+RussianYak:SetCoalition("sameonly")
+
+-- Explicitly exclude Senaki from possible departures and destinations.
+RussianYak:ExcludedAirports("Anapa-Vityazevo" , "Krymsk" , "Novorossiysk")
+
+-- We also change the livery of these groups. If a table of liveries is given, each spawned group gets a random livery.
+RussianYak:Livery({"Georgian Airlines", "Aeroflot", "Olympic Airways", "Ukrainian" })
+
+
+-- This makes aircraft respawn at their destination airport instead of another random airport.
+RussianYak:ContinueJourney()
+
+
+
+
+-- Spawn three aircraft.
+RussianYak:Spawn(3)
+
+RAT:StatusReports(false) 
