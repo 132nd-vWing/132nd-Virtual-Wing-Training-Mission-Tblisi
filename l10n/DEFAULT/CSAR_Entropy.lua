73eb37414bd258e4b6d9b7fde001acfa5ef91d0e
132nd-Entropy <ber17118@gmail.com>
2017-11-17 17:27:26 +0100

    fix: fixed ejected pilot not being transportable

--- l10n/DEFAULT/CSAR_Entropy.lua
+++ l10n/DEFAULT/CSAR_Entropy.lua
@@ -1,9 +1,47 @@
+--CTLD Modified Functions--
+-- took this function from CTLD to spawn a CTLD-compatible group at a vec3 instead of a zone
+function ctld.spawnGroupAtPoint_EJECT(_groupSide, _number, _point, _searchRadius)
+
+    local _country
+    if _groupSide == "red" then
+        _groupSide = 1
+        _country = 0
+    else
+        _groupSide = 2
+        _country = 2
+    end
+
+    if _searchRadius < 0 then
+        _searchRadius = 0
+    end
+
+    local _groupDetails = ctld.generateTroopTypes(_groupSide, _number, _country)
+
+    local _droppedTroops = ctld.spawnDroppedGroup(_point, _groupDetails, false, _searchRadius);
+
+    if _groupSide == 1 then
+        table.insert(ctld.droppedTroopsRED, _droppedTroops:getName())
+    else
+        table.insert(ctld.droppedTroopsBLUE, _droppedTroops:getName())
+    end
+end
+--/CTLD Modified Functions--
+
+
+
+
+
+
+
+
 
 
 function Skinnybeacon()
-SkinnyPos = Skinny:GetRandomVec3(15)
 ctld.beaconCount = ctld.beaconCount + 1
-ctld.createRadioBeacon(SkinnyPos, 2, 2, "Ejected Pilot" .. ctld.beaconCount - 1, 120)
+ctld.createRadioBeacon(CrashPos, 2, 2, "Ejected Pilot" .. ctld.beaconCount - 1, 120)
+--SkinnyPos = Skinny:GetRandomVec3(15)
+--ctld.beaconCount = ctld.beaconCount + 1
+--ctld.createRadioBeacon(SkinnyPos, 2, 2, "Ejected Pilot" .. ctld.beaconCount - 1, 120)
 end
 
 SetClient = SET_CLIENT:New():FilterCoalitions("blue"):FilterCategories("plane"):FilterStart()
@@ -12,16 +50,21 @@ SetClient:HandleEvent (EVENTS.Ejection)
 function SetClient:OnEventEjection(EventData)
 -- MessageToAll("MAYDAY MAYDAY MAYDAY",15)
    SkinnyPilot = EventData.IniUnit   
-   CrashPos = SkinnyPilot:GetVec2()
-   SkinnyZone = ZONE_RADIUS:New("SkinnyZone",CrashPos,3000)
-   Skinny = SPAWN:New("Skinny"):SpawnInZone(SkinnyZone,true)
-   table.insert(ctld.extractableGroups, Skinny)
+   CrashPos = SkinnyPilot:GetRandomVec3(3000)
+   
+   ctld.spawnGroupAtPoint_EJECT("blue", {aa=1}, CrashPos, 10) -- This calls the modified CTLD function to spawn a single Manpad as 'downed pilot'
+   
+
+   
+--   SkinnyZone = ZONE_RADIUS:New("SkinnyZone",CrashPos,3000)
+--   Skinny = SPAWN:New("Skinny"):SpawnInZone(SkinnyZone,true)
+--   table.insert(ctld.extractableGroups, Skinny)
 
 SkinnyMenu = MENU_COALITION:New(coalition.side.BLUE,"radio option for Ejected pilot")
 MENU_COALITION_COMMAND:New(coalition.side.BLUE, "Activate Homing Beacon",SkinnyMenu,Skinnybeacon)
 
 -- Let's get a reference to Skinny's RADIO
-EmergencyRadio = Skinny:GetRadio()  
+EmergencyRadio = SkinnyPilot:GetRadio()  
 
 -- Now, we'll set up the ntransmission
 EmergencyRadio:SetFileName("ejection.ogg")  -- We first need the file name of a sound,
@@ -30,4 +73,8 @@ EmergencyRadio:SetModulation(radio.modulation.AM) -- and a modulation
 EmergencyRadio:SetLoop(false) -- optional set true to loop it
 EmergencyRadio:Broadcast()
 end 
+
+
+
+
 
